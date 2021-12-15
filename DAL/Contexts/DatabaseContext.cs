3d5f48892c689ab72236323ee29cd136bca921f3
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2021-12-15 18:16:26 +0330

    Receive ConnectionString from App.config

--- DAL/Contexts/DatabaseContext.cs
+++ DAL/Contexts/DatabaseContext.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +12,11 @@ namespace DAL.Contexts
 {
     public class DatabaseContext:DbContext
     {
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-ABNJIE3\MSSQLSERVER2019; Initial Catalog=ContactDb; Integrated Security=true;");
+            string connectionString = ConfigurationManager.AppSettings.Get("LocalConnectionString");
+            optionsBuilder.UseSqlServer(connectionString);
         }
         public DbSet<Contact> Contacts { get; set; }
     }
